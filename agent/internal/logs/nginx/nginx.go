a324febcfe8a45e1dccbccf372b777203bf5ed80
khazit <ali@chunkcorp.com>
2025-08-22 18:36:03 +0100

    Raw line processing func is now defined inside each log collector

--- agent/internal/logs/nginx/nginx.go
+++ agent/internal/logs/nginx/nginx.go
@@ -2,7 +2,10 @@ package nginx
 
 import (
 	"context"
+	"fmt"
 	"path/filepath"
+	"regexp"
+	"time"
 
 	"agent/internal/collection"
 	"agent/internal/logs"
@@ -37,8 +40,7 @@ func (c *NginxLogCollector) Discover() []collection.LogSource {
 func (c *NginxLogCollector) Start(ctx context.Context, out chan<- logs.LogEntry) error {
 	// Initialize the runner on the first start
 	if c.runner == nil {
-		regexString := `\[(?P<timestamp>\d{2}/\w{3}/\d{4}:\d{2}:\d{2}:\d{2} [+-]\d{4})\]`
-		runner, err := logs.NewTailRunner(c.pattern, regexString, c.name)
+		runner, err := logs.NewTailRunner(c.pattern, c.processLogLine)
 		if err != nil {
 			return err
 		}
@@ -52,4 +54,41 @@ func (c *NginxLogCollector) Stop() error {
 		return nil
 	}
 	return c.runner.Stop()
+}
+
+func (c *NginxLogCollector) processLogLine(logLine string) (logs.LogEntry, error) {
+	entry := logs.LogEntry{
+		Source: c.name,
+		Text:   logLine,
+		Labels: make(map[string]string),
+	}
+
+	// Match labels
+	regex := `\[(?P<timestamp>\d{2}/\w{3}/\d{4}:\d{2}:\d{2}:\d{2} [+-]\d{4})\]`
+	re := regexp.MustCompile(regex)
+	matches := re.FindStringSubmatch(logLine)
+	if matches == nil {
+		return logs.LogEntry{}, fmt.Errorf("can't match any label in logline")
+	}
+
+	// Extract named capture groups directly
+	for i, name := range re.SubexpNames() {
+		if i != 0 && name != "" && i < len(matches) {
+			entry.Labels[name] = matches[i]
+		}
+	}
+
+	// Parse the timestamp into time.Time
+	timestampStr, ok := entry.Labels["timestamp"]
+	if ok {
+		layout := "02/Jan/2006:15:04:05 -0700"
+		timestamp, err := time.Parse(layout, timestampStr)
+		if err != nil {
+			return logs.LogEntry{}, fmt.Errorf("failed to parse timestamp: %v", err)
+		}
+		entry.Timestamp = timestamp.UnixMilli()
+		delete(entry.Labels, "timestamp")
+	}
+
+	return entry, nil
 }
